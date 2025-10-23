c683f29df2d53f40922f22196ddd76bf22209ed0
Pedro Silva <ph.barbosilva@proton.me>
2025-10-23 00:13:30 -0300

    refactored deprecated config

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ["avatars.githubusercontent.com"],
+    remotePatterns: [{
+      protocol: 'https',
+      hostname: 'avatars.githubusercontent.com'
+    }],
   }
 };
 
